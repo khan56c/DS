cda6b7129df761407e3f3d8a005279c343b1bf7c
Md Hasanur Roshid Khan <136918973+khan56c@users.noreply.github.com>
2023-06-23 04:33:47 +0600

    Update indexing.js

--- array/indexing.js
+++ array/indexing.js
@@ -1,3 +1,6 @@
-let indexing=[  openar,[tamim,liton,santo,naim,[joy,jakir,saif,bro],one_down,
+let indexing=[  "openar",["tamim","liton","santo","naim",["joy","jakir","saif","bro"],"miraj"],
+              "one_down",["sakib","santo"],"middle_order",["mr9","mahmudullah","afif","rabbi","mosaddek","miraz"],
+              "all_rounder",["sakib","miraz","saifuddin"],"spinner",["sakib","nasum","miraz","opu","mahadi","taijul"],
+              "pacer",["taskin","ebadot","mustafix","hasan","xFactor"]
   
 ];
